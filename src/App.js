9b94f53f9e6279ccd34b59149cd413ed9117c3f3
HK Yooun <yooun.hk@gmail.com>
2021-02-10 14:51:27 +0900

    Add history feature

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ export default function App() {
     const [left, setLeft] = useState(genRand(2, 9));
     const [right, setRight] = useState(genRand(1, 9));
     const [outputString, setOutputString] = useState('');
+    const [history, setHistory] = useState([]);
+    const [winCount, setWinCount] = useState(0);
 
     const inputEl = useRef();
-
 
     const changeHandler = (e) => {
         setUserInput(parseInt(e.target.value));
@@ -24,26 +25,49 @@ export default function App() {
         const answer = left * right;
         if (answer === userInput) {
             setOutputString(`정답입니다!`);
+            setWinCount(winCount + 1);
+            console.log(winCount);
         } else {
-            setOutputString(`제출: ${userInput}, 정답: ${answer}\n
-            틀렸습니다.`);
+            setOutputString(`틀렸습니다.`);
         }
+        setHistory(prevState => [...prevState, {
+            left: left,
+            right: right,
+            answer: answer,
+            userInput: userInput
+        }]);
         inputEl.current.value = null;
         setLeft(genRand(2, 9));
         setRight(genRand(1, 9));
         inputEl.current.focus();
     }
 
+    const renderHistory = () =>
+        history.map((e, index) => {
+            return (
+                <>
+                    <li key={index}>
+                        {`Q${index + 1}: ${e.left} * ${e.right} = ${e.answer} `}
+                        <br/>
+                        {`제출: ${e.userInput} 결과: ` + (e.answer === e.userInput ? '정답' : '오답')}
+                    </li>
+                </>
+            );
+        });
+
     return (
         <div className="App-header">
+            <div className="output">{outputString}</div>
             <div className="question">{left} * {right} = ?</div>
-            <div className="break"></div>
             <form className="input-form" onSubmit={submitHandler}>
                 <input ref={inputEl} type="text" onChange={changeHandler}/>
                 <button type="submit">제출</button>
             </form>
-            <div className="break"></div>
-            <div className="output">{outputString}</div>
+            <div>{`정답 횟수: ${winCount}`}</div>
+            <div>History</div>
+            <div>
+                {renderHistory()}
+            </div>
         </div>
     );
 }
